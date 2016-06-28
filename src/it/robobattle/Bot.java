e3988f0ef475a0a8c4f7f99a7cc8758d7b61547f
alex <alex@algos.it>
2016-06-29 00:34:45 +0200

    .

--- src/it/robobattle/Bot.java
+++ src/it/robobattle/Bot.java
@@ -2,6 +2,9 @@ package it.robobattle;
 
 import java.awt.*;
 
+/**
+ * Interfaccia che descrive le funzioni di un Bot.
+ */
 public interface Bot {
 
 	/**
@@ -27,12 +30,13 @@ public interface Bot {
 	/**
 	 * Inverte le lettere di una parola.
 	 * @param in la parola in ingresso
-	 * @return la parola scritta al contrario.
+	 * @return la parola scritta al contrario
 	 */
 	String invertWord(String in);
 
 	/**
 	 * Calcola la checksum di una parola.
+	 * </p>
 	 * Determina il valore di ogni carattere e restituisce la somma totale.
 	 * @param in la parola in ingresso
 	 * @return la checksum
@@ -41,24 +45,27 @@ public interface Bot {
 
 	/**
 	 * Decripta una parola in base a una chiave data.
-	 *
-	 * @param in  la parola da decrittare
-	 * @param key la chiave
-	 * @return la parola decrittata.
 	 * <p/>
 	 * Ogni carattere ha un valore numerico short corrispondente.
 	 * Essendo sia char che short valori a 16 bit, sono intercambiabili.
+	 * <p/>
 	 * Per ottenere il numero dal carattere o il carattere dal
 	 * numero basta un casting, come da esempio:
-	 * char c1 = 'x';
-	 * short n = (short)c1;
-	 * char c2 = (char)n;
+	 * </p>
+	 * <code>
+	 * char c1 = 'x';<br>
+	 * short n = (short)c1;<br>
+	 * char c2 = (char)n;<br>
+	 * </code>
 	 * <p/>
-	 * La chiave ricevuta contiene solo lettere minuscole.
-	 * La somma dei valori delle vocali contenute nella chiave dà un numero segreto.
-	 * Alle lettere in posizione pari va aggiunto il numero segreto.
-	 * Dalle lettere in posizione dispari va sottratto il numero segreto.
-	 * La prima lettera si intende alla posizione 1.
+	 * La somma dei valori delle vocali contenute nella chiave dà un numero segreto.<br>
+	 * Alle lettere in posizione pari va aggiunto il numero segreto.<br>
+	 * Dalle lettere in posizione dispari va sottratto il numero segreto.<br>
+	 * La prima lettera si intende alla posizione 1.<br>
+	 * La chiave ricevuta contiene solo lettere minuscole.<br>
+	 * @param in  la parola da decrittare
+	 * @param key la chiave
+	 * @return la parola decrittata.
 	 */
 	String decryptWord(String in, String key);
 
--- src/it/robobattle/BotTester.java
+++ src/it/robobattle/BotTester.java
@@ -2,115 +2,136 @@ package it.robobattle;
 
 /**
  * Tester delle implementazioni di Bot.
+ * </p>
+ * Questo esempio istanzia un tester con un dato bot, ed esegue un test:
+ * </p>
+ * <code>
+ * BotTester tester=new BotTester(new MyBot());<br>
+ * tester.startTest(Tests.CALC_CKECKSUM, 100);<br>
+ * </code>
  */
 public class BotTester {
 
     private Bot bot;
-    int logLevel=0;
+    int logLevel = 0;
 
+    /**
+     * Costruttore.
+     *
+     * @param bot il bot da testare
+     */
     public BotTester(Bot bot) {
         this.bot = bot;
-        logLevel=1; // max = 2
+        logLevel = 1; // max = 2
     }
 
     /**
      * Avvia una sessione di test.
-     * @param test il test da eseguire
+     * </p>
+     *
+     * @param test         il test da eseguire
      * @param quanteParole il numero di blocchi di parole da testare (1000 parole per blocco)
-     * */
-    public void startTest(Tests test, int quanteParole){
+     */
+    public void startTest(Tests test, int quanteParole) {
 
-        int quanteElab=0;
-        int quanteOK=0;
-        long totNanos=0;
-        long i=0;
+        int quanteElab = 0;
+        int quanteOK = 0;
+        long totNanos = 0;
+        long i = 0;
         String[] strings = creaStringhe(quanteParole);
-        for(String word : strings){
+        for (String word : strings) {
             i++;
 
 
-            long jobStartNanos=0;
-            long jobLengthNano=0;
-            Object expected=null;
-            Object obtained=null;
+            long jobStartNanos = 0;
+            long jobLengthNano = 0;
+            Object expected = null;
+            Object obtained = null;
 
 
-            switch (test){
+            switch (test) {
 
                 case SORT_WORD:
                     expected = BotAlgorithms.sortWord(word);
-                    jobStartNanos=System.nanoTime();
+                    jobStartNanos = System.nanoTime();
                     obtained = bot.sortWord(word);
-                    jobLengthNano=System.nanoTime()-jobStartNanos;
+                    jobLengthNano = System.nanoTime() - jobStartNanos;
                     break;
 
                 case INVERT_WORD:
                     expected = BotAlgorithms.invertWord(word);
-                    jobStartNanos=System.nanoTime();
+                    jobStartNanos = System.nanoTime();
                     obtained = bot.invertWord(word);
-                    jobLengthNano=System.nanoTime()-jobStartNanos;
+                    jobLengthNano = System.nanoTime() - jobStartNanos;
                     break;
 
                 case CALC_CKECKSUM:
                     expected = BotAlgorithms.calcChecksum(word);
-                    jobStartNanos=System.nanoTime();
+                    jobStartNanos = System.nanoTime();
                     obtained = bot.calcChecksum(word);
-                    jobLengthNano=System.nanoTime()-jobStartNanos;
+                    jobLengthNano = System.nanoTime() - jobStartNanos;
                     break;
 
                 case DECRYPT_WORD:
                     String key = "abcd";
                     expected = BotAlgorithms.decryptWord(word, key);
-                    jobStartNanos=System.nanoTime();
+                    jobStartNanos = System.nanoTime();
                     obtained = bot.decryptWord(word, key);
-                    jobLengthNano=System.nanoTime()-jobStartNanos;
+                    jobLengthNano = System.nanoTime() - jobStartNanos;
                     break;
 
             }
 
-            totNanos+=jobLengthNano;
+            totNanos += jobLengthNano;
 
-            boolean passed=false;
-            if(obtained!=null){
-                passed=(obtained.equals(expected));
+            boolean passed = false;
+            if (obtained != null) {
+                passed = (obtained.equals(expected));
             }
 
-            if(passed) {
+            if (passed) {
                 quanteOK++;
-                if(logLevel>=2){
-                    System.out.println(i+" "+word+" OK - "+jobLengthNano+" nanos");
+                if (logLevel >= 2) {
+                    System.out.println(i + " " + word + " OK - " + jobLengthNano + " nanos");
                 }
-            }else{
-                if(logLevel>=1){
-                    System.out.println("atteso: "+expected+" ricevuto: "+obtained);
+            } else {
+                if (logLevel >= 1) {
+                    System.out.println("atteso: " + expected + " ricevuto: " + obtained);
                 }
             }
 
             quanteElab++;
         }
 
-        long totMillis=totNanos/1000000;
-        long quantePerSec = quanteElab/totMillis*1000;
-        String s=bot.getNome()+" "+test.getTestName()+" "+quanteElab+" elaborate, "+quanteOK+" OK, "+totMillis+" ms, "+quantePerSec+" wps";
+        long totMillis = totNanos / 1000000;
+        long quantePerSec = quanteElab / totMillis * 1000;
+        String s = bot.getNome() + " " + test.getTestName() + " " + quanteElab + " elaborate, " + quanteOK + " OK, " + totMillis + " ms, " + quantePerSec + " wps";
         System.out.println(s);
 
     }
 
-    private String[] creaStringhe(int quantiBlocchi){
-        String[] stringhe = new String[Data.STRINGHE.length*quantiBlocchi];
-        for(int i=0; i<quantiBlocchi;i++){
-            for(int j=0; j<Data.STRINGHE.length;j++){
-                int k=(i*Data.STRINGHE.length)+j;
-                stringhe[k]=Data.STRINGHE[j];
+    private String[] creaStringhe(int quantiBlocchi) {
+        String[] stringhe = new String[Data.STRINGHE.length * quantiBlocchi];
+        for (int i = 0; i < quantiBlocchi; i++) {
+            for (int j = 0; j < Data.STRINGHE.length; j++) {
+                int k = (i * Data.STRINGHE.length) + j;
+                stringhe[k] = Data.STRINGHE[j];
             }
         }
         return stringhe;
     }
 
 
-
-
+    /**
+     * Regola il livello di log.
+     * </p>
+     * 0=normale (solo log finale)<br>
+     * 1=info (log dei soli errori e log finale, questo è il default)<br>
+     * 2=debug (log di tutte le operazioni e log finale)<br>
+     *
+     * @param i il livello di log
+     */
     public void setLogLevel(int i) {
-        this.logLevel=i;
+        this.logLevel = i;
     }
 }
